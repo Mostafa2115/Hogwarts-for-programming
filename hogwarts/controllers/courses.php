1b56ef21d0bc339bbeb838ec2feab37f0f41d837
mazen <mazenmashal@gmail.com>
2025-03-20 12:11:56 +0200

    add some controllers & authentiacation

--- hogwarts/controllers/courses.php
+++ hogwarts/controllers/courses.php
@@ -1,3 +1,21 @@
 <?php
-    $header = 'Courses';
-    require 'views/courses.view.php';
+
+    class CoursesController
+    {
+        private $db;
+        
+        public function __construct($db)
+        {
+            $this->db = $db;
+        }
+        public function getCourses()
+        {
+            $header = 'Courses';
+            $courses = $this->db->query('SELECT * FROM courses')->fetchAll(PDO::FETCH_OBJ);
+            return require 'views/courses.view.php';
+        }
+        public function addCourse($id)
+        {
+            return require 'views/course.view.php';
+        }
+    }
--- hogwarts/controllers/leaderboard.php
+++ hogwarts/controllers/leaderboard.php
@@ -1,3 +1,16 @@
 <?php
-    $header = 'Leader Board';
-    require 'views/leaderboard.view.php';
+
+    class LeaderboardController
+    {
+        private $db;
+        public function __construct($db)
+        {
+            $this->db = $db;
+        }
+        public function getLeaderboard()
+        {
+            $header = 'Leaderboard';
+            $students = $this->db->query('SELECT * FROM students')->fetchAll(PDO::FETCH_OBJ);
+            return require 'views/leaderboard.view.php';
+        }
+    }
