f63a76ad5017f1780cb74c5b93d48b05b2c27a38
Mostafa_Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-03-28 20:30:24 +0200

    Add files via upload

---
+++ hogwarts/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'dbname' => 'hogwarts',
+        'user' => 'root',
+        'password' => '7888'
+    ],
+    'path' => '/php/Hogwarts-for-programming/hogwarts/controllers'
+];
